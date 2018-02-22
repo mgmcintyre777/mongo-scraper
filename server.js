4ca12208714bedb8d3b6b9b418324e1ee4d214e8
Matt McIntyre <mgmcintyre777@gmail.com>
2018-02-22 13:34:12 -0500

    final final

--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-// ----------------------------------------------------
-// --- S E R V E R . J S ------------------------------
-// ----------------------------------------------------
-// Entry point into application.  
-// Dan Orlovsky
-
-// INCLUDES
 const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
@@ -13,18 +6,15 @@ const mongoose = require('mongoose');
 const app = express();
 
 
-// SETS UP OUR STATIC DIRECTORY
+
 app.use(express.static("public"))
 
-// INCLUDES THE BODYPARSER MIDDLEWARE
 app.use(bodyParser.urlencoded({extended: false }));
 app.use(bodyParser.json());
 
-// SETS UP OUR VIEW ENGINE
 app.engine('handlebars', exphbs({defaultLayout: 'main' }))
 app.set('view engine', 'handlebars');
 
-// CONFIGURE MONGOOSE
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost:27017/news-scraper");
 
