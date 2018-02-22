e5278322a7aaa6cb563f47bed7c4a051b7e1b539
mgmcintyre777 <mgmcintyre777@gmail.com>
2018-02-22 13:26:57 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,9 +1,3 @@
-// ----------------------------------------------------
-// --- I N D E X . J S --------------------------------
-// ----------------------------------------------------
-// Allows us to simply include our [models] folder
-// Dan Orlovsky
-
 module.exports = {
     Article: require('./Article'),
     Note: require('./Note'),
